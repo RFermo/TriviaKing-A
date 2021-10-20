b563aee4fa634bbf9414a6f4a459f2d27ab56cb7
Randolph <rfermo9807@gmail.com>
2021-10-20 16:08:04 -0500

    Almost finished landing page

---
+++ client/src/LandingPage/Description.jsx
@@ -0,0 +1,51 @@
+import { FaCheckSquare } from "react-icons/fa";
+
+const Description = () => {
+    return (
+        <div className="w-1/2 min-h-screen bg-gradient-to-b from-black to-gray-700 flex items-center justify-center">
+            <div className="w-4/5 mx-auto">
+                <h1 className="text-white text-5xl text-center leading-relaxed font-inter">A unique trivia application that will test every fiber of your knowledge.</h1>
+
+                <div className="flex flex-col space-y-5 mt-10">
+                    <div className="flex space-x-4 items-center">
+                        <div>
+                            <FaCheckSquare className="w-6 h-6 text-white" />
+                        </div>
+                        <div>
+                            <h5 className="font-georama text-lg text-white">Distinctive lifeline system</h5>
+                        </div>
+                    </div>
+
+                    <div className="flex space-x-4 items-center">
+                        <div>
+                            <FaCheckSquare className="w-6 h-6 text-white" />
+                        </div>
+                        <div>
+                            <h5 className="font-georama text-lg text-white">Ability to see your friends' scores</h5>
+                        </div>
+                    </div>
+
+                    <div className="flex space-x-4 items-center">
+                        <div>
+                            <FaCheckSquare className="w-6 h-6 text-white" />
+                        </div>
+                        <div>
+                            <h5 className="font-georama text-lg text-white">Secure registration/login system</h5>
+                        </div>
+                    </div>
+
+                    <div className="flex space-x-4 items-center">
+                        <div>
+                            <FaCheckSquare className="w-6 h-6 text-white" />
+                        </div>
+                        <div>
+                            <h5 className="font-georama text-lg text-white">Huge database of questions</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Description;
