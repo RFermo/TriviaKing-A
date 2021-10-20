a84d840664b5ce6a05ea6873be2a4801b9a3a923
Javi OP <javiornelas@gmail.com>
2021-10-19 23:19:32 -0500

    initial express setup

---
+++ server/Routes/register.js
@@ -0,0 +1,9 @@
+export async function Register() {
+    try {
+        console.log('You are trying to register');
+    }
+    catch (error) {
+        assert.fail(`There was some error: ${error}`)
+    }
+};
+
