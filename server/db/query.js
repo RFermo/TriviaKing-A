14a06bc228c024353206b2162a1f76805400410b
Javier Ornelas <javiornelas@gmail.com>
2021-11-27 23:19:57 -0600

    more backend profile updates

--- server/db/query.js
+++ server/db/query.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const db = require('../db/connect');
 
-dbQuery = (sql, values) => {
+const dbQuery = (sql, values) => {
     let query = mysql.format(sql, values);
     return new Promise( (resolve, reject) => {
         db.query(query, (error, data) => {
@@ -11,4 +11,14 @@ dbQuery = (sql, values) => {
     });
 };
 
-module.exports = dbQuery;
+const dbQueryAll = (sql, values) => {
+    let query = mysql.format(sql, values);
+    return new Promise( (resolve, reject) => {
+        db.query(query, (error, data) => {
+            if (error) return reject(error);
+            resolve(data);
+        });
+    });
+};
+
+module.exports = {dbQuery, dbQueryAll};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -10,5 +10,8 @@ module.exports = (app) => {
     app.delete('/user/logout', User.logout);
     app.get('/user/find/', User.findOne);
     app.put('/user/update_profile', User.authorized, User.updateProfile);
+    app.get('/user/get_profile', User.authorized, User.getProfile);
+    app.get('/user/get_all_profiles', User.authorized, User.getAllProfiles);
+    app.get('/user/get_highscores', User.authorized, User.getHighscores);
     app.post('/user/protected', User.authorized, User.protected);
 };
