60d04ec18874b5f0e17a9ecd5763d332b2a2262e
Randolph <rfermo9807@gmail.com>
2021-11-28 14:26:53 -0600

    Fixed loading screen

--- client/src/Loading.jsx
+++ client/src/Loading.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FaFrown } from 'react-icons/fa';
+
 
 const Loading = () => {
+    
     return (
-        <div className="min-h-screen bg-gray-800"></div>
+        <div className="min-h-screen flex justify-center items-center bg-gray-800">
+            <div className="w-85 lg:w-11/12 flex flex-col items-center space-y-5">
+                <FaFrown className="w-16 h-16 mx-auto text-gray-200" />
+                <h1 className="text-gray-200 text-xl md:text-2xl leading-normal font-inter font-bold">The app seems to be down buddy. Perhaps it's a good time to get knowledge somewhere else while we fix it?</h1>
+            </div>
+        </div>
     );
 };
 
